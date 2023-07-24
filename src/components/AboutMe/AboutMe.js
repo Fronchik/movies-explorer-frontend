7dfd98ea52a4550356aac4c9cc4268e4630530c9
Anna <A.V.Nazareva@yandex.ru>
2023-07-24 20:16:56 +0300

    add component AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,23 @@
+import aboutMePhoto from '../../images/photo.jpg';
+import React from 'react';
+import './AboutMe.css';
+
+function AboutMe() {
+    return (
+        <section className="aboutMe">
+            <h2 className="aboutMe__title">Студент</h2>
+            <div className="aboutMe__container">
+                <div className="aboutMe__info">
+                    <h3 className="aboutMe__info-name">Виталий</h3>
+                    <p className="aboutMe__info-prof">Фронтенд-разработчик, 30 лет</p>
+                    <p className="aboutMe__info-text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <p className="aboutMe__info-web">Github</p>
+                </div>
+                <img src={aboutMePhoto} alt="Фото студента" className="aboutMe__info-photo" />
+            </div>
+        </section>
+    );
+}
+
+export default AboutMe;
