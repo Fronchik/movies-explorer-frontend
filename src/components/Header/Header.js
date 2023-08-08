18a4d0b0b8d4ad26e98010edcb18b76d22c2dda9
Anna <A.V.Nazareva@yandex.ru>
2023-08-08 22:40:36 +0300

    btn header close

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,55 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 import headerLogo from '../../images/logo.svg';
 import profile from '../../images/profile.svg';
 import navigationIcon from '../../images/navigationIcon.svg'
 
 function Header({ email, loggedIn, onSignOut }) {
+    const [isMenuOpened, setMenuOpened] = React.useState(false);
+
     return (
-        <header className="header">
-            <Link to="/" aria-label="Логотип" type="submit">
-                <img src={headerLogo} alt="Логотип" className="header__logo" />
-            </Link>
-            {loggedIn &&
-                <div className="header__movies">
-                    <Link to="/movies" aria-label="Ссылка на фильмы" className="header__link-movies">
-                        Фильмы
-                    </Link>
-                    <Link to="/saved-movies" aria-label="Ссылка на сохранённые фильмы" className="header__link-saved-movies">
-                        Сохранённые фильмы
-                    </Link>
-                </div>
-            }
-            <div className="header__buttons">
-                {!loggedIn ?
-                    <>
-                        <Link to="/signup" aria-label="Кнопка регистрации">
-                            <button className="header__button" aria-label="Кнопка регистрации" type="button">
-                                Регистрация
-                            </button>
-                        </Link>
-                        <Link to="/signin" aria-label="Кнопка войти">
-                            <button className="header__button-green" aria-label="Кнопка войти" type="button">
-                                Войти
-                            </button>
-                        </Link>
-                    </>
-                    :
-                    <Link to="/profile" aria-label="Кнопка аккаунта">
-                        <button className="header__profile" aria-label="Кнопка аккаунта" type="button">
-                            <img src={profile} alt="Профиль" />
-                            Аккаунт
-                        </button>
-                    </Link>
-                }
-            </div>
-            {loggedIn &&
-                <Link to="/navigation" aria-label="ссылка на меню">
-                    <img src={navigationIcon} alt="навигация" className="header__navigation" />
+        <>
+            <header className="header">
+                <Link to="/" aria-label="Логотип" type="submit">
+                    <img src={headerLogo} alt="Логотип" className="header__logo" />
                 </Link>
+                {loggedIn &&
+                    <div className="header__movies">
+                        <Link to="/movies" aria-label="Ссылка на фильмы" className="header__link-movies">
+                            Фильмы
+                        </Link>
+                        <Link to="/saved-movies" aria-label="Ссылка на сохранённые фильмы" className="header__link-saved-movies">
+                            Сохранённые фильмы
+                        </Link>
+                    </div>
+                }
+                <div className="header__buttons">
+                    {!loggedIn ?
+                        <>
+                            <Link to="/signup" aria-label="Кнопка регистрации">
+                                <button className="header__button" aria-label="Кнопка регистрации" type="button">
+                                    Регистрация
+                                </button>
+                            </Link>
+                            <Link to="/signin" aria-label="Кнопка войти">
+                                <button className="header__button-green" aria-label="Кнопка войти" type="button">
+                                    Войти
+                                </button>
+                            </Link>
+                        </>
+                        :
+                        <Link to="/profile" aria-label="Кнопка аккаунта">
+                            <button className="header__profile" aria-label="Кнопка аккаунта" type="button">
+                                <img src={profile} alt="Профиль" />
+                                Аккаунт
+                            </button>
+                        </Link>
+                    }
+                </div>
+                {loggedIn &&
+                    <button aria-label="Кнопка меню" onClick={() => setMenuOpened(true)} className="header__navigation-close">
+                        <img src={navigationIcon} alt="навигация" className="header__navigation" />
+                    </button>
+                }
+            </header>
+            {isMenuOpened &&
+                <Navigation onClose={() => setMenuOpened(false)} />
             }
-        </header>
+        </>
     );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,13 +4,13 @@ import './Navigation.css';
 import close from '../../images/close.svg';
 import profile from '../../images/profile.svg';
 
-function Navigation() {
+function Navigation({ onClose }) {
     return (
         <section className="navigation">
             <div className="navigation__content">
-                <Link to="/" aria-label="Закрыть">
+                <button aria-label="Закрыть" onClick={onClose} className="navigation__btn-close">
                     <img src={close} alt="Закрыть" className="navigation__close" />
-                </Link>
+                </button>
                 <div className="navigation__links">
                     <Link to="/" aria-label="Ссылка на фильмы" className="navigation__link">
                         Главная
@@ -23,7 +23,7 @@ function Navigation() {
                     </Link>
                 </div>
                 <Link to="/profile" aria-label="Кнопка аккаунта" className="navigation__profile">
-                    <button className="header__profile" aria-label="Кнопка аккаунта" type="button">
+                    <button className="header__profile header__profile-on" aria-label="Кнопка аккаунта" type="button">
                         <img src={profile} alt="Профиль" />
                         Аккаунт
                     </button>
