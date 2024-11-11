2ce8b757952be76e18144e84aed6ca96e0238e3d
Anna <A.V.Nazareva@yandex.ru>
2024-11-11 16:37:26 +0200

    change components About me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,10 +8,10 @@ function AboutMe() {
             <h2 className="aboutMe__title">Студент</h2>
             <div className="aboutMe__container">
                 <div className="aboutMe__info">
-                    <h3 className="aboutMe__info-name">Виталий</h3>
-                    <p className="aboutMe__info-prof">Фронтенд-разработчик, 30 лет</p>
-                    <p className="aboutMe__info-text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <h3 className="aboutMe__info-name">Анна</h3>
+                    <p className="aboutMe__info-prof">Фронтенд-разработчик</p>
+                    <p className="aboutMe__info-text">Я родилась и живу в Москве. Получила высшее образование в Российском экономическом университете имени Г.В. Плеханова.
+                        Люблю фотографировать. С 2022 года я изучаю веб-программирование. Мне нравится создавать красивые и функциональные веб-сайты.</p>
                     <p className="aboutMe__info-web">Github</p>
                 </div>
                 <img src={aboutMePhoto} alt="Фото студента" className="aboutMe__info-photo" />
